4b60e9ddb8ce45c693edcc2b55e13acaacf8ed76
Lance Ma <lancema507@gmail.com>
2022-04-17 23:53:21 -0400

    added a few more videos

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-const generateHTML =  (websiteName) => {
+const generateHTML0 =  (websiteName) => {
 
     return ` 
     
-  
-
     <body>
 
     <div style = "display: flex; align-items:center; justify-content:center;" class = "title">
@@ -14,42 +12,173 @@ const generateHTML =  (websiteName) => {
 
 
    <div style = "display: flex;  align-items:center; justify-content:center" class = "vid">
-
    
     <iframe width="560" height="315" src="https://www.youtube.com/embed/cnrxJtJcSew?start=91&autoplay=1&showinfo=0&controls=0&loop=1"   
     allow="autoplay;" frameborder="0" allowfullscreen></iframe>
 
     </div>
 
-    
     </body>
-
     `;
 };
 
+const generateHTML1 =  (websiteName) => {
+
+    return ` 
+    
+    <body>
+
+    <div style = "display: flex; align-items:center; justify-content:center;" class = "title">
+
+    <h1 style = "color:red; font-size:64px;"> GET BACK TO STUDYING! NO SOCIAL MEDIA! </h1>
+
+   </div>
+
+   <div style = "display: flex;  align-items:center; justify-content:center" class = "vid">
+   
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/iJAxeafvn8Y?autoplay=1&showinfo=0&controls=0&loop=1"   
+    allow="autoplay;" frameborder="0" allowfullscreen></iframe>
+
+    </div>
+
+    </body>
+    `;
+};
+
+const generateHTML2 =  (websiteName) => {
+
+    return ` 
+    
+    <body>
+
+    <div style = "display: flex; align-items:center; justify-content:center;" class = "title">
+
+    <h1 style = "color:red; font-size:64px;"> GET BACK TO STUDYING! NO SOCIAL MEDIA! </h1>
+
+   </div>
+
+   <div style = "display: flex;  align-items:center; justify-content:center" class = "vid">
+   
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/YG4iTGjuoKw?start=61&autoplay=1&showinfo=0&controls=0&loop=1"   
+    allow="autoplay;" frameborder="0" allowfullscreen></iframe>
+
+    </div>
+
+    </body>
+    `;
+};
+
+
+function getRandomNumber(max) {
+    return Math.floor(Math.random() * max);
+}
 
 switch (window.location.hostname) {
 
 
     case "www.youtube.com":
-        document.body.innerHTML = generateHTML("Youtube");
-        break;
+
+        var randNum = getRandomNumber(3)
+        console.log(randNum);
+
+        if (randNum == 0) {
+            document.body.innerHTML = generateHTML0("Youtube");
+            break;
+        }
+        else if (randNum == 1) {
+            document.body.innerHTML = generateHTML1("Youtube");
+            break;
+        }
+        else if (randNum == 2) {
+            document.body.innerHTML = generateHTML2("Youtube");
+            break;
+        }
+
     
     case "www.twitter.com":
-        document.body.innerHTML = generateHTML("Twitter");
-        break;
+        var randNum = getRandomNumber(3)
+        console.log(randNum);
+
+        if (randNum == 0) {
+            document.body.innerHTML = generateHTML0("Youtube");
+            break;
+        }
+        else if (randNum == 1) {
+            document.body.innerHTML = generateHTML1("Youtube");
+            break;
+        }
+        else if (randNum == 2) {
+            document.body.innerHTML = generateHTML2("Youtube");
+            break;
+        }
 
     case "www.facebook.com":
-        document.body.innerHTML = generateHTML("Facebook");
-        break;
+        var randNum = getRandomNumber(3)
+        console.log(randNum);
+
+        if (randNum == 0) {
+            document.body.innerHTML = generateHTML0("Youtube");
+            break;
+        }
+        else if (randNum == 1) {
+            document.body.innerHTML = generateHTML1("Youtube");
+            break;
+        }
+        else if (randNum == 2) {
+            document.body.innerHTML = generateHTML2("Youtube");
+            break;
+        }
     
     case "www.instagram.com":
-        document.body.innerHTML = generateHTML("Instagram");
-        break;
+        var randNum = getRandomNumber(3)
+        console.log(randNum);
+
+        if (randNum == 0) {
+            document.body.innerHTML = generateHTML0("Youtube");
+            break;
+        }
+        else if (randNum == 1) {
+            document.body.innerHTML = generateHTML1("Youtube");
+            break;
+        }
+        else if (randNum == 2) {
+            document.body.innerHTML = generateHTML2("Youtube");
+            break;
+        }
 
     case "www.tiktok.com":
-        document.body.innerHTML = generateHTML("Tiktok");
-        break;
+        var randNum = getRandomNumber(3)
+        console.log(randNum);
+
+        if (randNum == 0) {
+            document.body.innerHTML = generateHTML0("Youtube");
+            break;
+        }
+        else if (randNum == 1) {
+            document.body.innerHTML = generateHTML1("Youtube");
+            break;
+        }
+        else if (randNum == 2) {
+            document.body.innerHTML = generateHTML2("Youtube");
+            break;
+        }
+    
+    case "www.reddit.com":
+        var randNum = getRandomNumber(3)
+        console.log(randNum);
+
+        if (randNum == 0) {
+            document.body.innerHTML = generateHTML0("Youtube");
+            break;
+        }
+        else if (randNum == 1) {
+            document.body.innerHTML = generateHTML1("Youtube");
+            break;
+        }
+        else if (randNum == 2) {
+            document.body.innerHTML = generateHTML2("Youtube");
+            break;
+        }
 
    
 }
